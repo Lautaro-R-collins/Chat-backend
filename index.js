e408d805f301852df19917c29dca345dbbb4f380
Lautaro Rodriguez Collins <135086443+Lautaro-R-collins@users.noreply.github.com>
2024-03-20 18:42:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 4000;
 
 require('dotenv').config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
 
 app.use(cors());
 app.use(express.json());
@@ -21,26 +21,26 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    const { message, user: sender, type, members } = req.body;
+    // const { message, user: sender, type, members } = req.body;
 
-    if(type === 'message.new') {
-        members
-            .filter((member) => member.user_id !== sender.id)
-            .forEach(({ user }) => {
-                if(!user.online) {
-                    client.messages
-                    .create({
-                     body: 'Your appointment is coming up on July 21 at 3PM',
-                      from: 'whatsapp:+14155238886',
-                    to: 'whatsapp:+5492616908835'
-                    })
-                    .then(message => console.log(message.sid))
-                        .done();
-                   }
-                       })
+    // if(type === 'message.new') {
+    //     members
+    //         .filter((member) => member.user_id !== sender.id)
+    //         .forEach(({ user }) => {
+    //             if(!user.online) {
+    //                 client.messages
+    //                 .create({
+    //                  body: 'Your appointment is coming up on July 21 at 3PM',
+    //                   from: 'whatsapp:+14155238886',
+    //                 to: 'whatsapp:+5492616908835'
+    //                 })
+    //                 .then(message => console.log(message.sid))
+    //                     .done();
+    //                }
+    //                    })
 
-            return res.status(200).send('Message sent!');
-    }
+    //         return res.status(200).send('Message sent!');
+    // }
 
     return res.status(200).send('Not a new message request');
 });
